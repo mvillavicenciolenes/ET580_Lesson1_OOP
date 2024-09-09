d674c807bc5f83fd061ab20e06f09f1b26a88dec
mvillavicenciolenes <143456253+mvillavicenciolenes@users.noreply.github.com>
2024-09-09 23:00:31 +0000

    Update 2 09/09/2024 End of Lesson

--- Lesson1_Object_Oriented_Basics/5_constructors.cpp
+++ Lesson1_Object_Oriented_Basics/5_constructors.cpp
@@ -1,5 +1,6 @@
 // S. Trowbridge 2024
 #include <iostream>
+using namespace std;
 
 class id {
 private:
@@ -9,6 +10,9 @@ public:
     // constructors                                 // constructor functions are automatically called to initialize object data when the object is created
     id(): name("noname"), num(0) {}                 // default constructor:             assigns default values when creating an object
     id(std::string n, int x): name(n), num(x) {}    // parameterized constructor:       assigns specified values when creating an object
+    id(string n1):name(n1), num(-1) {}
+    id(int num1):name("Default Name"), num(num1) {}
+
 
     std::string getName() { return name; }
     int getNum() { return num; }
@@ -22,9 +26,14 @@ int main()
 
     id student_1;                                   // instantiate (create) an object and call the default constructor (use default values)
     id student_2("John Smith", 1111);               // instantiate (create) an object using a parameterized constructor (specify values)
+    id student_A("John Smith"); 
+    id student_B(1234);
 
     std::cout  << student_1.getName() << " " << student_1.getNum() << "\n";
     std::cout  << student_2.getName() << " " << student_2.getNum() << "\n";
+    std::cout  << student_A.getName() << " " << student_A.getNum() << "\n";
+    std::cout  << student_B.getName() << " " << student_B.getNum() << "\n";
+
 
     std::cout << std::endl;
     return 0;
--- Lesson1_Object_Oriented_Basics/6_member_vs_non-member.cpp
+++ Lesson1_Object_Oriented_Basics/6_member_vs_non-member.cpp
@@ -14,6 +14,11 @@ public:
     int getNum() { return num; }
     void setName(std::string n) { name = n; }
     void setNum(int x) { num = x; }
+
+    void print2()                          
+{
+    std::cout  << getName() << " " << getNum() << "\n";   
+}
 };
 
 // non-member functions are declared/defined outside of the class definition and require an object reference argument to access data
@@ -33,6 +38,11 @@ int main()
 
     print(student_1);                           // non-member function syntax ( functionName( callingObject) )
     print(student_2);
+    std::cout << std::endl;  
+
+
+    student_1.print2();                           
+    student_2.print2();
 
     std::cout << std::endl;
     return 0;
