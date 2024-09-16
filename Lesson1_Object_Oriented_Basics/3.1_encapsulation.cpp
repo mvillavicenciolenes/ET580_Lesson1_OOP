57ed6507202cc1631e6d60d58ceb6a0c6686b4ce
“mvillavicenciolenes” <michael.lenes.mv@outlook.com>
2024-09-16 12:01:12 -0400

    constructors practice

--- Lesson1_Object_Oriented_Basics/3.1_encapsulation.cpp
+++ Lesson1_Object_Oriented_Basics/3.1_encapsulation.cpp
@@ -1,8 +1,35 @@
 #include <iostream>
 #include <string>
 
+class Stove{
+    private:
+        int temperature = 0;
+
+    public:
+        int getTemperature(){
+            return temperature;
+        }
+
+    void setTemperature(int temperature){
+        if(temperature < 0){
+            this -> temperature = 0;
+        }
+        else if(temperature >= 10){
+            this -> temperature = 10;
+        }
+        else{
+            this -> temperature = temperature;
+        }
+        this -> temperature = temperature;
+    }
+};
 
 int main(){
-    
+    Stove stove;
+
+    //stove.temperature = 10000000000;
+    stove.setTemperature(10000000);
+
+    std::cout << "The temperature setting is " << stove.getTemperature();
     return 0;
 }
